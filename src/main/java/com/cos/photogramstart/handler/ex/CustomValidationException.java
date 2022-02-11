c46396164cd181eb7590f016e2039a62c9db2b51
chinoPha <komsez92@gmail.com>
2022-02-11 23:53:45 +0900

    220211
    text

--- src/main/java/com/cos/photogramstart/handler/ex/CustomValidationException.java
+++ src/main/java/com/cos/photogramstart/handler/ex/CustomValidationException.java
@@ -2,6 +2,7 @@ package com.cos.photogramstart.handler.ex;
 
 import java.util.Map;
 
+//에러딴에 데이터만이 아니라 에러문구도 확인하고자 만듦
 public class CustomValidationException extends RuntimeException{
 	// 시리얼 번호 - 객체를 구분할 때 !! JVM한테 중요함.
 	private static final long serialVersionUID = 1L;
