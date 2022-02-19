228ad80ef689f77e37a34f929c4f4acbaaf43d49
chinoPha <komsez92@gmail.com>
2022-02-20 03:21:17 +0900

    220219

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.handler.ex.CustomException;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.utill.Script;
@@ -23,8 +24,18 @@ public class ControllerExceptionHandler {
 		// 1. 클라이언트에게 응답할 때는 Script 좋음. - 통신을 '브라우저'로 받는거(클라이언트)
 		// 2. Ajax통신 - CMRespDto 가 좋음 - 개발자가 자바스크립트 코드로 서버쪽으로 던져서 응답받는거(개발자)
 		// 3. Android통신 - CMRespDto 가 좋음 - 응답을 안드로이드앱에서 개발자가 받는거(개발자)
-		return Script.back(e.getErroMap().toString());
-				/*return new CMRespDto<Map<String,String>>(-1, e.getMessage(),e.getErroMap());*/
+		if(e.getErroMap() == null) {
+			return Script.back(e.getMessage());
+		}else {
+			return Script.back(e.getErroMap().toString());
+			/*return new CMRespDto<Map<String,String>>(-1, e.getMessage(),e.getErroMap());*/
+		}
+		
+	}
+	
+	@ExceptionHandler(CustomException.class) 
+	public String exception(CustomException e) {
+		return Script.back(e.getMessage());		
 	}
 	@ExceptionHandler(CustomValidationApiException.class) // RuntimeException이 발동하는 모든 Exception을 validationException함수가 가로챔
 	public ResponseEntity<?> validationApiException(CustomValidationApiException e) { //<>제네릭 사용시 <?>를 사용하면 알아서 찾아줌.
---
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -0,0 +1,53 @@
+package com.cos.photogramstart.service;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.image.ImageRepository;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class ImageService {
+	
+	//db에 저장하기위해 
+	private final ImageRepository imageRepository;
+	
+	@Value("${file.path}")		//org.springframework, ${"application.yml에 등록된 이름"}
+	private String uploadFolder; //
+	
+	//사진 업로드를 하는 함수 생성
+	@Transactional
+	public void 사진업로드(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
+		//사진 파일의 이름 중복방지
+		UUID uuid = UUID.randomUUID();// uuid
+		String imageFileName = uuid+"_"+imageUploadDto.getFile().getOriginalFilename(); // 실제 파일 네임이 들어감. 1.jpg
+		System.out.println("이미지 파일 이름: "+imageFileName);
+		
+		//실제 저장 경로 - 저장용도
+		Path imageFilPath = Paths.get(uploadFolder+imageFileName); // 경로와 파일명
+		
+		//통신, I/O(하드디스크에 기록을 하거나 읽을때) - > 예외가 발생할 수 있다.
+		try {
+			Files.write(imageFilPath, imageUploadDto.getFile().getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// image 테이블에 저장
+		Image image = imageUploadDto.toEntity(imageFileName, principalDetails.getUser()); // 이미지 파일 이름: cde1326a-edf7-4d33-ba38-a6337eb92320_are.jpg
+		imageRepository.save(image);
+		
+		//System.out.println(imageEntity.toString());
+	}
+}
